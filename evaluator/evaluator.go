a276012edef772201b01a68747cb54bb45475ec7
Yiping Liao <yipingliao@gmail.com>
2021-02-17 10:43:13 -0800

    Evaluate booleans; add null value.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,18 +5,26 @@ import (
 	"Monkey_Interpreter/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
-
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -30,4 +38,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
